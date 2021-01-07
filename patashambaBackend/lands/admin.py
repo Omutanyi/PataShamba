9d8743f4868911ce3410b8b3d7b5f39507ec0444
Javier Omutanyi <javieromutanyi@gmail.com>
2021-01-07 20:04:40 +0300

    Lands models created, migrations not creating tables

--- patashambaBackend/lands/admin.py
+++ patashambaBackend/lands/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import land
 
 class LandAdmin(admin.ModelAdmin): 
-  list_display = ('land_id', 'description', 'likes', 'price', 'negotiable', 'size', 'pub_date')
+  list_display = ('land_id', 'description', 'location', 'likes','size', 'pub_date')
 
 # Register your models here.
 admin.site.register(land, LandAdmin)
